e1fa17380ce816efd2e9d077caaf60e263ba20fa
namht-web-dev <hocdieuhay99@gmail.com>
2025-06-26 22:01:34 +0700

    css without responsive

--- src/JobInfo.jsx
+++ src/JobInfo.jsx
@@ -3,10 +3,10 @@ import Duties from "./Duties";
 const JobInfo = ({ jobs, currentJob }) => {
   const { company, dates, duties, title } = jobs[currentJob];
   return (
-    <article className="iob-info">
-      <h2 className="title">{title}</h2>
-      <p className="company">{company}</p>
-      <p className="dates">{dates}</p>
+    <article className="job-info">
+      <h2 className="job-title">{title}</h2>
+      <span className="job-company">{company}</span>
+      <p className="job-dates">{dates}</p>
       {duties.map((duty, index) => (
         <Duties key={index} text={duty} />
       ))}
